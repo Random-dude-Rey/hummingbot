e0ec7df3f961f64e805d0cfecacbe00b34eb559f
Daniel Tan <daniel@hummingbot.io>
2022-04-14 19:56:12 +0800

    (refactor) integrate ClientOrderTracker into GatewayEVMAMM Connector

--- hummingbot/connector/gateway_in_flight_order.py
+++ hummingbot/connector/gateway_in_flight_order.py
@@ -1,8 +1,9 @@
+import copy
 from decimal import Decimal
-from typing import Optional
+from typing import Any, Optional, Tuple
 
 from hummingbot.core.data_type.common import OrderType, TradeType
-from hummingbot.core.data_type.in_flight_order import InFlightOrder, OrderState
+from hummingbot.core.data_type.in_flight_order import InFlightOrder, OrderState, OrderUpdate
 
 
 class GatewayInFlightOrder(InFlightOrder):
@@ -34,14 +35,6 @@ class GatewayInFlightOrder(InFlightOrder):
         self._cancel_tx_hash: Optional[str] = None
 
     @property
-    def is_pending_approval(self) -> bool:
-        return self.current_state in {OrderState.PENDING_APPROVAL}
-
-    @property
-    def is_approval_request(self) -> bool:
-        return self.current_state in {OrderState.PENDING_APPROVAL, OrderState.APPROVED}
-
-    @property
     def gas_price(self) -> Decimal:
         return self._gas_price
 
@@ -64,3 +57,67 @@ class GatewayInFlightOrder(InFlightOrder):
     @cancel_tx_hash.setter
     def cancel_tx_hash(self, cancel_tx_hash):
         self._cancel_tx_hash = cancel_tx_hash
+
+    @property
+    def attributes(self) -> Tuple[Any]:
+        return copy.deepcopy(
+            (
+                self.client_order_id,
+                self.trading_pair,
+                self.order_type,
+                self.trade_type,
+                self.price,
+                self.amount,
+                self.exchange_order_id,
+                self.current_state,
+                self.leverage,
+                self.position,
+                self.executed_amount_base,
+                self.executed_amount_quote,
+                self.creation_timestamp,
+                self.last_update_timestamp,
+                self.nonce,
+                self.gas_price,
+                self.cancel_tx_hash,
+            )
+        )
+
+    @property
+    def is_pending_approval(self) -> bool:
+        return self.current_state in {OrderState.PENDING_APPROVAL}
+
+    @property
+    def is_approval_request(self) -> bool:
+        """
+        A property attribute that returns `True` if this `GatewayInFlightOrder` is in fact a token approval request.
+
+        :return: True if this `GatewayInFlightOrder` is in fact a token approval request, otherwise it returns False
+        :rtype: bool
+        """
+        return self.current_state in {OrderState.PENDING_APPROVAL, OrderState.APPROVED}
+
+    def update_with_order_update(self, order_update: OrderUpdate) -> bool:
+        """
+        Updates the in flight order with an order update
+        return: True if the order gets updated otherwise False
+        """
+        if (order_update.client_order_id != self.client_order_id
+                and order_update.exchange_order_id != self.exchange_order_id):
+            return False
+
+        prev_data = self.attributes
+
+        if self.exchange_order_id is None and order_update.exchange_order_id is not None:
+            self.update_exchange_order_id(order_update.exchange_order_id)
+
+        self.current_state = order_update.new_state
+        self.nonce = order_update.misc_updates.get("nonce", None)
+        self.fee_asset = order_update.misc_updates.get("fee_asset", None)
+        self.gas_price = order_update.misc_updates.get("gas_price", None)
+
+        updated: bool = prev_data != self.attributes
+
+        if updated:
+            self.last_update_timestamp = order_update.update_timestamp
+
+        return updated
